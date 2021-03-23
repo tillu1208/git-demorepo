6d039892a8f22e045b66c1fa56b5a4903e5734f5
Sumanth <48374330+tillu1208@users.noreply.github.com>
2021-03-23 07:15:09 +0530

    added ADD method

--- GitDemo/src/main/Calculator.java
+++ GitDemo/src/main/Calculator.java
@@ -1,6 +1,9 @@
 package main;
 
 public class Calculator {
+	public static int add(int n1,int n2){
+		return n1+n2;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Calculator");
