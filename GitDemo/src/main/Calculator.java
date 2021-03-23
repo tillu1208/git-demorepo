d4eeccc42154ae2d71e564f4ee2023c07536c9ed
Sumanth <48374330+tillu1208@users.noreply.github.com>
2021-03-23 09:53:26 +0530

    added Square function

--- GitDemo/src/main/Calculator.java
+++ GitDemo/src/main/Calculator.java
@@ -10,6 +10,9 @@ public class Calculator {
 	public static int prod(int n1,int n2){
 		return n1*n2;
 	}
+	public static int square(int n1){
+		return n1*n1;
+	}
 	public static void main(String[] args) {
 		System.out.println("Calculator");
 	}
