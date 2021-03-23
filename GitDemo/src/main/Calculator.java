0c50f3c6e859b52435af1d984fdb9c819a573e99
Sumanth <48374330+tillu1208@users.noreply.github.com>
2021-03-23 09:50:11 +0530

    added Div and Mod function

--- GitDemo/src/main/Calculator.java
+++ GitDemo/src/main/Calculator.java
@@ -10,6 +10,12 @@ public class Calculator {
 	public static int prod(int n1,int n2){
 		return n1*n2;
 	}
+	public static int div(int n1,int n2){
+		return n1/n2;
+	}
+	public static int mod(int n1,int n2){
+		return n1%n2;
+	}
 	public static void main(String[] args) {
 		System.out.println("Calculator");
 	}
