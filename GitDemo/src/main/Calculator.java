bd1bf2cd091fff24af92a8be6a0443d224aac14b
Sumanth <48374330+tillu1208@users.noreply.github.com>
2021-03-23 10:18:38 +0530

    added master cube function

--- GitDemo/src/main/Calculator.java
+++ GitDemo/src/main/Calculator.java
@@ -10,20 +10,14 @@ public class Calculator {
 	public static int prod(int n1,int n2){
 		return n1*n2;
 	}
-
-	public static int div(int n1,int n2){
-		return n1/n2;
-	}
-	public static int mod(int n1,int n2){
-		return n1%n2;
-	}
 	public static int square(int n1){
 		return n1*n1;
-
+	}
+	public static int cube(int n1){
+		return n1*n1*n1;
 	}
 	public static void main(String[] args) {
 		System.out.println("Calculator");
-		System.out.println(add(10,20));
 	}
-
+	
 }
