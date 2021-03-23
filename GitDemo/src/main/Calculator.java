c077ec13f1a4107775603f9feb4c6ed42d5f411c
Sumanth <48374330+tillu1208@users.noreply.github.com>
2021-03-23 10:26:11 +0530

    local branch 1 added fives

--- GitDemo/src/main/Calculator.java
+++ GitDemo/src/main/Calculator.java
@@ -22,6 +22,10 @@ public class Calculator {
 		return n1*n1*n1*n1;
 
 	}
+	public static int fives(int n1){
+		return n1*n1*n1*n1*n1;
+
+	}
 	public static void main(String[] args) {
 		System.out.println("Calculator");
 	}
